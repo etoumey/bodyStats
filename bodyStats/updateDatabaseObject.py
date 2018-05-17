d197fe11ecdbe09f650784659ace5ebbfdcae994
Julian Toumey <jtoumey@gmail.com>
2018-05-17 17:31:13 -0500

    Update files

--- bodyStats/updateDatabaseObject.py
+++ bodyStats/updateDatabaseObject.py
@@ -1,20 +1,33 @@
-
-
+# Class Definitions
 
 class GCData:
   dowData = []
   data = []
 
-
-
 # rhr class
-class rhr(GCData):
-  
+class Rhr(GCData):
+
+#def parseRHR(dayRHR, rhr):
+  def parseRHR(self):
+    # open a sample file and ignore the first two lines (headers)
+    f = open('rhr_29apr2017.csv', 'r')
+    rhrData = f.readlines()[2:]
+
+    # loop over the file lines and parse the data
+    for line in rhrData:
+      dayTemp, rhrTemp = line.split(",")
+
+      dayRHR.append(dayTemp)
+      rhr.append(rhrTemp.rstrip('\n'))
+
+    f.close()
+
   def printInfo(self):
     print self.dowData
 
 # sleep class
-class sleep(GCData):
+class Sleep(GCData):
+
 
 
 
@@ -26,3 +39,35 @@ print rhrUpdate.dowData
 print '\n\n'
 
 rhrUpdate.printInfo()
+
+
+
+
+
+
+def parseSleep(daySlp, sleep):
+  f = open('slp_09jul2017.csv', 'r')
+  sleepData = f.readlines()[2:]
+
+  for line in sleepData:
+    # the sleep file has column 2: sleep as a float, column3: sleep as hr:min
+    dayTemp, sleepFractionTemp, sleepClockTemp = line.split(",")
+
+    daySlp.append(dayTemp)
+    sleep.append(sleepFractionTemp.rstrip('\n'))
+
+  f.close()
+
+def parseStep(dayStp, steps):
+  f = open('stp_09jul2017.csv', 'r')
+  stepData = f.readlines()[2:]
+
+  for line in stepData:
+    # step file has column 2: actual steps, column 3: goal steps
+    dayTemp, stepTemp, stepGoalTemp = line.split(",")
+
+    dayStp.append(dayTemp)
+    steps.append(stepTemp.rstrip('\n'))
+
+  f.close()
+
