911cccaf9e2ad9ad6b99b5a6aa324dd7b0fe82ed
Eliot Toumey <etoumey@gmail.com>
2018-05-27 20:49:54 -0400

    what is selenium

---
+++ selenium/seleniumTest.py
@@ -0,0 +1,12 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+opts = Options()
+opts.set_headless()
+assert opts.headless
+browser = Firefox(options=opts)
+browser.get('https://duckduckgo.com')
+search_form = browser.find_element_by_id('search_form_input_homepage')
+search_form.send_keys('hi')
+search_form.submit()
+results = browser.find_elements_by_class_name('result')
+print(results[0].text)
