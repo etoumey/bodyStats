770852e9e005c77957fd9ea0741466d331946dc5
etoumey <etoumey@gmail.com>
2019-03-04 21:44:39 -0500

    GeckoDriver, selenium, and firefox all updated

--- selenium/seleniumTest.py
+++ selenium/seleniumTest.py
@@ -4,29 +4,32 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 from os import getcwd
 from datetime import date
+import time
 
 def queryCredentials():
 
-    fid = open("credentials.pass", 'r')
-    cred = fid.readlines()
-    fid.close()
-
-    return cred
+	fid = open("credentials.pass", 'r')
+	cred = fid.readlines()
+	fid.close()
+	print "Credentials sucessfully loaded..."
+	return cred
 
 def downloadReport(browser):
 	# Inside reports window, switch to default frame
-    browser.switch_to_default_content()
-    # Wait for page to load
-    element = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, "pageContainer"))
-    )
+	browser.switch_to_default_content()
+	# Wait for page to load
+	time.sleep(5)
+	element = WebDriverWait(browser, 10).until(
+		EC.presence_of_element_located((By.ID, "pageContainer"))
+	)
 
-    exportXpath = '//*[@id="pageContainer"]/div/div[1]/div[2]/div/button'
-    browser.find_element_by_xpath(exportXpath).click()
-    arrowXpath = '//*[@id="pageContainer"]/div/div[2]/div[2]/div[1]/div/span[1]/button[1]'
-    browser.find_element_by_xpath(arrowXpath).click()
+	exportXpath = '//*[@id="pageContainer"]/div/div[1]/div[2]/div/button'
+	browser.find_element_by_xpath(exportXpath).click()
+	arrowXpath = '//*[@id="pageContainer"]/div/div[2]/div[2]/div[2]/div/span[1]/button[1]'
+	browser.find_element_by_xpath(arrowXpath).click()
 
 def reportMerge(downloadDir):
 	today = str(date.today())
@@ -36,62 +39,70 @@ def reportMerge(downloadDir):
 	year = today[0]
 
 	
+def browserInit(downloadDir):
+	
+	# Set Firefox preferences -- specifically to download *.csv files w/o raising a confirmation dialog box
+	ffProfile = webdriver.FirefoxProfile()
+	ffProfile.set_preference('browser.download.folderList', 2) # custom location
+	ffProfile.set_preference('browser.download.manager.showWhenStarting', False)
+	ffProfile.set_preference('browser.download.dir', downloadDir)
+	ffProfile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 
+	#Setup browser as headless
+	opts = Options()
+	opts.headless = False
 
+	# Instantiate a Firefox browser object with the above-specified profile settings
+	print "Browser preferences configured"
+	browser = webdriver.Firefox(ffProfile, options = opts)
+	print "Launching browser"
+	
+	return browser
 
 
 
 
 def main():
+	downloadDir = getcwd()
+	credentials = queryCredentials()
+	#   Head to garmin connect login page
+	browser = browserInit(downloadDir)
 
-    credentials = queryCredentials()
-    downloadDir = getcwd()
-    # Set Firefox preferences -- specifically to download *.csv files w/o raising a confirmation dialog box
-    ffProfile = webdriver.FirefoxProfile()
-    ffProfile.set_preference('browser.download.folderList', 2) # custom location
-    ffProfile.set_preference('browser.download.manager.showWhenStarting', False)
-    ffProfile.set_preference('browser.download.dir', downloadDir)
-    ffProfile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
+	browser.get('https://connect.garmin.com/modern/report/60/wellness/last_seven_days')
 
-    #Setup browser as headless
-    #opts = Options()
-    #opts.set_headless()
-    #assert opts.headless
-    #browser = Firefox(options=opts)
+	#input field is within an iframe
+	frame = browser.find_element_by_id('gauth-widget-frame-gauth-widget')
+	browser.switch_to.frame(frame)
 
-    # Instantiate a Firefox browser object with the above-specified profile settings
-    browser = webdriver.Firefox(ffProfile)
+	element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "username")))
 
-    #   Head to garmin connect login page
-    browser.get('https://connect.garmin.com/modern/report/60/wellness/last_seven_days')
+	#populate username and password
+	try:
+		usernameField = browser.find_element_by_id("username")
+		usernameField.send_keys(credentials[0])
+		element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "password")))
 
-    #input field is within an iframe
-    frame = browser.find_element_by_id('gauth-widget-frame-gauth-widget')
-    browser.switch_to.frame(frame)
+		passwordField = browser.find_element_by_id("password")
+		passwordField.send_keys(credentials[1])
+		passwordField.submit()
+		print "Login Success"
+	except:
+		print "Login Failure"
 
-    element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "username")))
 
-    #populate username and password
-    usernameField = browser.find_element_by_id("username")
-    usernameField.send_keys(credentials[0])
-    element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "password")))
+	requiredWeeks = 5
+	for x in range(0,requiredWeeks):
+		#try:
+		downloadReport(browser)
+		print "Week %d Success!" % (x + 1)
+#		except:
+#			print "Download Failed, try again later"
+#			exit()
 
-    passwordField = browser.find_element_by_id("password")
-    passwordField.send_keys(credentials[1])
-    passwordField.submit()
+	browser.quit()
+	reportMerge(downloadDir)
 
-    requiredWeeks = 0
-    for x in range(0,requiredWeeks):
-        try:
-        	downloadReport(browser)
-        except:
-        	print "Download Failed, try again later"
-        	exit()
-
-    browser.quit()
-    reportMerge(downloadDir)
-
-    
+	
 
 if __name__ == "__main__":
-    main()
+	main()
