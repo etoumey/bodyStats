bf1cc18a1b5c10d7d050f5015fc42fec0e35668e
Julian Toumey <jtoumey@gmail.com>
2019-10-15 08:09:03 -0400

    Working in a homecooked command line arg tool.

--- bodyStats.py
+++ bodyStats.py
@@ -3,11 +3,41 @@ import parser
 import tripy
 import plot
 
+import sys
+
 
 def bodyStats():
 
-    # Wrapper for tripy: get keyboard input for desired action
     keyboardInputFlag = True
+    print(sys.argv)
+
+    # Test if the user has supplied the appropriate key for cmd line args
+    if sys.argv[1].startswith('-'):
+        # -f: Fetch
+        if 'f' in sys.argv[1]:
+            print("Fetching reports...")
+            reportFetch.reportFetch()
+
+        if 'p' in sys.argv[1]:
+            print("Parsing...")
+            parser.parser()
+
+        if 't' in sys.argv[1]:
+            print("Running tripy...")
+            tripy.tripy()
+
+        if 'l' in sys.argv[1]:
+            print("Plotting...")
+            plot.main()
+
+        keyboardInputFlag = False
+
+    else:
+        print("Invalid command line arguments. Continuing...")
+
+
+    # Wrapper for tripy: get keyboard input for desired action
+
 
     while keyboardInputFlag:
 
@@ -27,7 +57,7 @@ def bodyStats():
             plot.plot()
 
             keyboardInputFlag = False
-    
+
         elif plotFlag == 'n':
             keyboardInputFlag = False
 
