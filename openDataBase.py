3aca3c4ee76b35baa23d101528cad313b4e34e99
etoumey <etoumey@gmail.com>
2020-09-21 22:23:52 -0400

    noticed bug

--- openDataBase.py
+++ openDataBase.py
@@ -6,7 +6,7 @@ def openDataBase():
 	connection = sqlite3.connect('userData.db')
 
 	# Basic create if doesn't already exist logic 
-	sqlCreateTableUserData = """ CREATE TABLE IF NOT EXISTS userData (date text NOT NULL, RHR real, SLEEP real, STRESS real, STEPS real, ATL real, CTL real, TSS, real, PRIMARY KEY (date) ); """
+	sqlCreateTableUserData = """ CREATE TABLE IF NOT EXISTS userData (date text NOT NULL, RHR real, SLEEP real, STRESS real, STEPS real, ATL real, CTL real, TSS real, PRIMARY KEY (date) ); """
 	sqlCreateTableActivities = """ CREATE TABLE IF NOT EXISTS activities (date text NOT NULL, FILEPATH real, DIST real, ELEV real, ELAPSEDTIME real, PRIMARY KEY (date) ); """
 
 	cursor = connection.cursor()
